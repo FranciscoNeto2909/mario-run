7867a1e795d7026bc51387495f6270ce1661acb7
Francisco Neto <wantedshark@gmail.com>
2022-06-03 11:24:04 -0300

    add game musics

--- game/script.js
+++ game/script.js
@@ -2,15 +2,19 @@ const pipe = document.querySelector(".game-pipe")
 const btn_play = document.querySelector(".game-btn-play")
 const player = document.querySelector(".game-player")
 const clouds = document.querySelector(".game-clouds")
+const playerJumpAudio = document.querySelector(".game-sound--jump")
+const gameMusicAudio = document.querySelector(".game-sound--music")
+
 pipe.style.visibility = "hidden"
 
 let loop = true
+gameMusicAudio.src = "./game/sounds/game_sound.mp3"
+gameMusicAudio.play()
 
 function startGame() {
     pipe.style.visibility = "visible"
     pipe.style.animation = "pipeMove 1.3s linear infinite"
     clouds.style.animation = "cloudsMove 2.5s linear infinite"
-
     setInterval(() => {
         const pipePos = pipe.offsetLeft
         const playerPos = +window.getComputedStyle(player).bottom.replace("px", "")
@@ -32,6 +36,8 @@ function startGame() {
 }
 function jump() {
     player.style.animation = "jump 600ms linear"
+    playerJumpAudio.play()
+
     setTimeout(() => {
         player.style.animation = ""
     }, 500);
@@ -42,16 +48,18 @@ function sobreposicao(elemA, elemB) {
     const b = elemB.getBoundingClientRect()
 
     const horizontal = a.left + a.width >= b.left
-    && b.left + b.width >= a.left
+        && b.left + b.width >= a.left
 
     const vertical = a.top + a.height >= b.top
-    && b.top + b.height >= a.top
+        && b.top + b.height >= a.top
 
     return vertical && horizontal
 }
 
 function gameOver(pipePos, playerPos) {
     loop = false
+    gameMusicAudio.src = "./game/sounds/game_over_sound.mp3"
+    gameMusicAudio.play()
     pipe.style.animation = "none"
     pipe.style.left = `${pipePos}px`
     player.style.animation = "none"
